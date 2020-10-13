b0d2d8ffd3f930c4b625922da08fbd8b48e2488f
LanaLebedeva <lanalebedeva2020@gmai.com>
2020-10-13 20:50:02 +0300

    Fix bug in lstdelone()

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: lana <LanaLebedeva2020@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 13:46:29 by lana              #+#    #+#             */
-/*   Updated: 2020/10/13 18:28:09 by lana             ###   ########.fr       */
+/*   Updated: 2020/10/13 20:48:25 by lana             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 	{	
 		if (del)
 			del((*alst)->content, (*alst)->content_size);
-		free((*alst)->content);
-		(*alst)->content = NULL;
-		(*alst)->content_size = 0;
+		free(*alst);
+		*alst = NULL;
 	}		
 	return;
 }
